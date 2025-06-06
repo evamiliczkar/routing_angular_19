0b35f8d4e790ba368cc6ee26d38f6650c3f719ac
evamiliczka <evamiliczka@rarecrew.com>
2025-06-06 14:05:45 +0200

    Lesson274_finished

--- src/app/users/user-tasks/user-tasks.component.ts
+++ src/app/users/user-tasks/user-tasks.component.ts
@@ -1,5 +1,7 @@
-import { Component, computed, inject, input, Input } from '@angular/core';
+import { Component, computed, inject, input, Input, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { UsersService } from '../users.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user-tasks',
@@ -7,20 +9,41 @@ import { UsersService } from '../users.service';
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
 })
-export class UserTasksComponent {
- // userId = input.required<string>();
-// @Input({ required: true }) userId!: string;
-  private  usersService = inject(UsersService);
- 
-  @Input() set userId(value: string){
-    console.log('User ID set to:', value);
-    this.userId = value;
-    
-  }
-  
-  username = computed(() => {
-  
-    return this.usersService.users.find(user => user.id === this.userId)?.name || 'Unknwn User'});
+export class UserTasksComponent implements OnInit, OnDestroy {
+  // userId = input.required<string>();
+  //@Input({ required: true }) userId!: string;
+  // private  usersService = inject(UsersService);
 
+  // @Input() set userId(value: string){
+  //   this.userId = value;
+  // }
+  //userId = input.required<string>();
+  username = '';
+  constructor(private activatedRoute: ActivatedRoute, private usersService: UsersService) { };
+
+  // username = computed(() => {
+  // console.log('Computing username for userId:', this.userId);
+
+  //   return this.usersService.users.find(user => user.id === this.userId)?.name || 'Unknwn User'});
+private subscription : Subscription = new Subscription();
+
+  ngOnInit(): void {
+    console.log('UserTasksComponent initialized');
+    this.subscription = this.activatedRoute.paramMap.subscribe({
+      next: (params) => {
+        // handle params here
+        this.username = this.usersService.users.find(u => u.id === params.get('userId'))?.name || 'Unknown user';
+      }
+    });
+  }
+
+    ngOnDestroy() {
+      console.log('UserTasksComponent destroyed');
+      this.subscription.unsubscribe();
+    }
+
+
+  
 
 }
+
